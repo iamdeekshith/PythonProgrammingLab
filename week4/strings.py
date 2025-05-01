5951b1d58642600a3309aa56db0eaebfa0234867
iamdeekshith <vijayadharavath143@gmail.com>
2025-05-01 12:05:12 +0530

    Create strings.py

---
+++ week4/strings.py
@@ -0,0 +1,51 @@
+'''
+string1="ACE"
+string2="ENGINEERING COLLEGE"
+concatenated_string = string1 + ' ' + string2
+print(concatenated_string)
+'''
+
+
+'''
+my_string="DEEKSHITH, NAIK"
+print(my_string[0:9])
+print(my_string[10:])
+print(my_string[:9])
+print(my_string[-4:])
+'''
+
+'''
+my_string="ACE ENGINEERING COLLEGE"
+length=len(my_string)
+print(length)
+'''
+
+'''
+text="PYTHON LAB"
+print(text.lower())
+text="string"
+print(text.upper())
+'''
+
+'''
+text="   IIT KHARAGPUR    "
+print(text.strip())
+'''
+
+'''
+text="IIT ROORKE"
+print(text.split())
+text= "IIT,NIT,IIIT,GFITS"
+print(text.split(","))
+'''
+
+'''
+text="TEJAVATH RAKESH"
+print(text.replace("TEJAVATH","DHARAVATH"))
+'''
+
+'''
+char="DEEKSHITH NAIK"
+print(char.find("DEEKSHITH"))
+print(char.find("NAIK"))
+'''
